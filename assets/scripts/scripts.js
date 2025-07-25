272230ced2c3d926f5f7caea83038f757f858f87
nickruden <1nikitos.ru2018@gmail.com>
2025-07-25 17:19:49 +0300

    Блоки первой страницы

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,76 +1,153 @@
 // Красим шапку при скролле
 function colorHeaderToWhite() {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 50) {
-        header.classList.add('bg-white');
-    } else {
-        header.classList.remove('bg-white');
-    }
+  const header = document.querySelector(".header");
+  if (window.scrollY > 50) {
+    header.classList.add("bg-white");
+  } else {
+    header.classList.remove("bg-white");
+  }
 }
+
 window.addEventListener("scroll", () => {
-   colorHeaderToWhite();
+  colorHeaderToWhite();
 });
+
 
 // Попап
 Fancybox.bind("[data-fancybox]", {
-  compact:true,
+  compact: true,
   tpl: {
-    closeButton: '<button data-fancybox-close class="f-button is-close-btn" title="{{CLOSE}}"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.8764 18.1818L2.12132 31.9369L0 29.8155L13.7551 16.0605L9.53674e-07 2.3054L2.12132 0.184082L15.8764 13.9391L29.6315 0.184082L31.7528 2.3054L17.9977 16.0605L31.7528 29.8155L29.6315 31.9369L15.8764 18.1818Z" fill="#2169EF"/></svg></button>'
-  }
+    closeButton:
+      '<button data-fancybox-close class="f-button is-close-btn" title="{{CLOSE}}"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.8764 18.1818L2.12132 31.9369L0 29.8155L13.7551 16.0605L9.53674e-07 2.3054L2.12132 0.184082L15.8764 13.9391L29.6315 0.184082L31.7528 2.3054L17.9977 16.0605L31.7528 29.8155L29.6315 31.9369L15.8764 18.1818Z" fill="#2169EF"/></svg></button>',
+  },
 });
 
+
 // Маска ввода
-$('[type="tel"]').each(function(){
-	new IMask(this, {
-		mask: '+{7} (000) 000 00 00',
-		prepare: function (appended, masked) {
-          if (appended === '8' && masked.value === '') {
-            return '7';
-          }
-          return appended;
-        },
-	});
+$('[type="tel"]').each(function () {
+  new IMask(this, {
+    mask: "+{7} (000) 000 00 00",
+    prepare: function (appended, masked) {
+      if (appended === "8" && masked.value === "") {
+        return "7";
+      }
+      return appended;
+    },
+  });
 });
 
 // Моб.меню
-const menuButton = $('.button-menu');
-menuButton.on("click", function() {
-    $('body').toggleClass('menu-active');
+const menuButton = $(".button-menu");
+menuButton.on("click", function () {
+  $("body").toggleClass("menu-active");
 });
+
 
 // Accordion
-const accordionHeader = $('.accordion-header');
-const accordionContent = $('.accordion-content');
-accordionHeader.on("click", function() {
-    $(this).toggleClass('active');
-    $(this).next().slideToggle();
+$(document).ready(function() {
+  $(".my-accordion-header").each(function() {
+    if ($(this).hasClass("active")) {
+      $(this).next(".my-accordion-content").show();
+    } else {
+      $(this).next(".my-accordion-content").hide();
+    }
+  });
+
+  $(".my-accordion-header").on("click", function() {
+    $(this).toggleClass("active");
+    $(this).next(".my-accordion-content").stop(true, true).slideToggle();
+  });
 });
 
+
 // Slider cases
-const swiper = new Swiper('.cases__wrap .swiper', {
+const swiper = new Swiper(".cases__wrap .swiper", {
   slidesPerView: 1,
   spaceBetween: 20,
   navigation: {
-    nextEl: '.cases .swiper-btn-next',
-    prevEl: '.cases .swiper-btn-prev',
-  }
+    nextEl: ".cases .swiper-btn-next",
+    prevEl: ".cases .swiper-btn-prev",
+  },
 });
 
-const swiper2 = new Swiper('.thanks-letters .swiper', {
+const swiper2 = new Swiper(".thanks-letters .swiper", {
   slidesPerView: 1.15,
   spaceBetween: 20,
   navigation: {
-    nextEl: '.thanks-letters .swiper-btn-next',
-    prevEl: '.thanks-letters .swiper-btn-prev',
+    nextEl: ".thanks-letters .swiper-btn-next",
+    prevEl: ".thanks-letters .swiper-btn-prev",
   },
   breakpoints: {
     768: {
       slidesPerView: 2.15,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     992: {
-        slidesPerView: 3.15,
-        spaceBetween: 30,
-    }
-  }
+      slidesPerView: 3.1,
+      spaceBetween: 30,
+    },
+  },
+});
+
+const swiper3 = new Swiper(".long-work .swiper", {
+  slidesPerView: 1.14,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".long-work .swiper-btn-next",
+    prevEl: ".long-work .swiper-btn-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.12,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3.1,
+      spaceBetween: 40,
+    },
+  },
+});
+
+const swiper4 = new Swiper(".promotions .swiper", {
+  slidesPerView: 1.48,
+  spaceBetween: 20,
+  slidesOffsetAfter: 0,
+  watchOverflow: true,
+  navigation: {
+    nextEl: ".promotions .swiper-btn-next",
+    prevEl: ".promotions .swiper-btn-prev",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: "auto",
+      spaceBetween: 50,
+    },
+    1199: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  },
+});
+
+const swiper5 = new Swiper(".our-articles .swiper", {
+  slidesPerView: 1.19,
+  spaceBetween: 25,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
 });
